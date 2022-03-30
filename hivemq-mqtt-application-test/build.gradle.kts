b6bb1c915ea739aa5170493dfe150b811e527658
yannick <yannick.weber@hivemq.com>
2022-03-30 09:27:56 +0200

    adapted dependency versions

--- hivemq-mqtt-application-test/build.gradle.kts
+++ hivemq-mqtt-application-test/build.gradle.kts
@@ -11,14 +11,14 @@ repositories {
 
 dependencies {
     implementation("com.hivemq:hivemq-mqtt-client:1.3.0")
-    implementation("com.github.testcontainers.testcontainers-java:hivemq:244c77c")
     implementation("org.jetbrains:annotations:23.0.0")
 
-    testImplementation("ch.qos.logback:logback-classic:1.2.11")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
-    testImplementation("org.testcontainers:junit-jupiter:1.16.3")
-    testImplementation("org.testcontainers:testcontainers")
+    testImplementation("ch.qos.logback:logback-classic:1.2.11")
     testImplementation("org.mockito:mockito-core:4.4.0")
+    testImplementation("com.github.testcontainers.testcontainers-java:hivemq:master-SNAPSHOT")
+    testImplementation("org.testcontainers:junit-jupiter:1.16.3")
+    testImplementation("org.testcontainers:testcontainers:1.16.3")
 
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
