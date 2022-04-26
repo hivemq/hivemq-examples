9c341e8eecc5fc62b95b5d9c256b916f3b59f37e
yannick <yannick.weber@hivemq.com>
2022-04-26 17:29:02 +0200

    fixed testcontainers dependency

--- hivemq-mqtt-application-test/build.gradle.kts
+++ hivemq-mqtt-application-test/build.gradle.kts
@@ -14,10 +14,9 @@ dependencies {
     testImplementation("ch.qos.logback:logback-classic:1.2.11")
     testImplementation("org.mockito:mockito-core:4.4.0")
 
-    implementation("org.testcontainers:testcontainers-bom:1.17.0")
-    testImplementation("org.testcontainers:junit-jupiter")
-    testImplementation("org.testcontainers:hivemq")
-    testImplementation("org.testcontainers:testcontainers")
+    testImplementation("org.testcontainers:junit-jupiter:1.17.0")
+    testImplementation("org.testcontainers:hivemq:1.17.0")
+    testImplementation("org.testcontainers:testcontainers:1.17.0")
 
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
